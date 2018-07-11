f50bd77d187254151e18d0ebee680ad91bb039c2
Hiteshi Acharya <acharyahiteshi@gmail.com>
2018-07-11 19:03:02 -0400

    Add files via upload

--- Dynamic_Programming/LongestCommonSubSequence.java
+++ Dynamic_Programming/LongestCommonSubSequence.java
@@ -7,7 +7,7 @@ import java.util.Set;
 /*
  * LCS Problem Statement: Given two sequences, find the length of longest subsequence 
  * present in both of them. A subsequence is a sequence that appears in the same relative order, but not 
- * necessarily contiguous. For example, “abc”, “abg”, “bdf”, “aeg”, ‘”acefg”, .. etc are subsequences of “abcdefg”. 
+ * necessarily contiguous. For example, ï¿½abcï¿½, ï¿½abgï¿½, ï¿½bdfï¿½, ï¿½aegï¿½, ï¿½ï¿½acefgï¿½, .. etc are subsequences of ï¿½abcdefgï¿½. 
  * So a string of length n has 2^n different possible subsequences.
  * 
  */
@@ -18,52 +18,79 @@ public class LongestCommonSubSequence {
 		String str2="AEDFHR";
 
 		System.out.println("\nRecursive "+str1+" "+str2+" "+ LCSRecursive(str1,  str2, str1.length(),  str2.length(), "")+" "+LCSRecursive(str1,  str2, str1.length(),  str2.length(), "").length());
-		System.out.println("\nRecursive "+str1+" "+str2+" "+ LCSIterative2ForLoops(str1,  str2)+" "+LCSIterative2ForLoops(str1,  str2).length());
+		System.out.println("\nIterative "+str1+" "+str2+" "+ LCSIterative2ForLoops(str1,  str2)+" "+LCSIterative2ForLoops(str1,  str2).length());
+		System.out.println("\n2Darray "+str1+" "+str2+" "+ LCSIterative2DArray(str1,  str2)+" "+LCSIterative2DArray(str1,  str2).length());
 
 
 		str1="AGGTAB";
 		str2="GXTXAYB";
-		System.out.println("\nRecursive "+str1+" "+str2+" "+ LCSIterative2DArray(str1,  str2)+" "+LCSIterative2DArray(str1,  str2).length());
-
 		System.out.println("\nRecursive "+str1+" "+str2+" "+ LCSRecursive(str1,  str2, str1.length(),  str2.length(), "")+" "+LCSRecursive(str1,  str2, str1.length(),  str2.length(), "").length());
+		System.out.println("\n2Darray "+str1+" "+str2+" "+ LCSIterative2DArray(str1,  str2));
 
 
 
 
 	}
 	public static String LCSIterative2DArray(String str1, String str2){//O(n^2)
-		String lcs1="",lcs2="";
+		/*
+		 *     G X T X A Y B
+		 *   0 0 0 0 0 0 0 0
+		 * A 0 0 0 0 0 1 1 1
+		 * G 0 1 1 1 1 1 1 1
+		 * G 0 1 1 1 1 1 1 1
+		 * T 0 1 1 2 2 2 2 2 
+		 * A 0 1 1 2 2 3 3 3
+		 * B 0 1 1 2 2 3 3 4
+		 */
 
-		int[][] adj=new int[str1.toCharArray().length][str2.toCharArray().length];
+		String lcs="";
+		int m=str1.length();
+		int n=str2.length();
+		int[][] adj=new int[m+1][n+1];
 
-		int pos=0;
-		for(int i=0;i<str1.length();i++){
+		for(int i=0;i<m+1;i++){
 
-			for(int j=0;j<str2.length();j++){
-				if(str1.charAt(i)==str2.charAt(j)){
-
-					adj[i][j]=i+j;
-
-					
+			for(int j=0;j<n+1;j++){
+				if(i==0||j==0) {
+					adj[i][j]=0;
+				}
+				else if(str1.charAt(i-1)!=str2.charAt(j-1)){
+					adj[i][j]=adj[i][j-1]>adj[i-1][j]?adj[i][j-1]:adj[i-1][j];
 
 				}else{
-					adj[i][j]=0;
-				
+					adj[i][j]=adj[i-1][j-1]+1;
+
 				}
 
 			}
 		}
-
-		for(int i=0;i<str1.length();i++){
+		// print matrix
+		for(int i=0;i<m+1;i++){
 			System.out.println();
-			for(int j=0;j<str2.length();j++){
+			for(int j=0;j<n+1;j++){
 				System.out.print(" "+adj[i][j]);
 
 			}
 		}
+		System.out.println();
+		//print sequence
+		for(int i=m;i>0;i--) {
+			for(int j=n;j>0;j--) {
+				if(adj[i][j]==adj[i-1][j]||adj[i][j]==adj[i][j-1]) {
 
+				}
+				else if(adj[i][j]==(adj[i-1][j-1]+1)) {
+					lcs=str1.charAt(i-1)+lcs;
+//					System.out.println(str1.charAt(i-1));
+					i=i-1;
+				}
 
-		return "";
+			}	
+
+		}
+
+		System.out.print("length "+lcs.length());
+		return lcs;
 	}
 	public static String LCSIterative2ForLoops(String str1, String str2){//O(n^2)
 		String lcs1="",lcs2="";
