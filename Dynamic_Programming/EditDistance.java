c939e2b3e392e80d92a2c956e222499d103f78f6
Hiteshi Acharya <acharyahiteshi@gmail.com>
2018-07-11 14:07:49 -0400

    Add files via upload

---
+++ Dynamic_Programming/EditDistance.java
@@ -0,0 +1,53 @@
+package Dynamic_Programming;
+/*
+ * Given two strings str1 and str2 and below operations that can performed on str1. Find minimum number of edits (operations) required to convert ‘str1’ into ‘str2’.
+
+Insert
+Remove
+Replace
+ * 
+ */
+public class EditDistance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		String str1="geek",str2="gessek";
+		System.out.println(str1+" "+str2+" "+checkDistanceFromEnd(str1,str2,str1.length(),str2.length()));
+
+		str1="cat";str2="cut";
+		System.out.println(str1+" "+str2+" "+checkDistanceFromEnd(str1,str2,str1.length(),str2.length()));
+
+		str1="sunday";str2="saturday";
+		System.out.println(str1+" "+str2+" "+checkDistanceFromEnd(str1,str2,str1.length(),str2.length()));
+
+	}
+	public static int checkDistanceFromEnd(String str1,String str2,int m , int n){
+		//start from last
+		if(m==0)
+			return n;
+		if(n==0)
+			return m;
+
+		if(str1.charAt(m-1)==str2.charAt(n-1)){
+			return checkDistanceFromEnd(str1,str2,m-1,n-1);
+
+		}
+
+
+		return 1+min(checkDistanceFromEnd(str1,str2,m,n-1),checkDistanceFromEnd(str1,str2,m-1,n),checkDistanceFromEnd(str1,str2,m-1,n-1));
+	}
+	public static int min(int x,int y , int z){
+		if (x<=y && x<=z) return x;
+		if (y<=x && y<=z) return y;
+		else return z;
+	}
+	public static int max(int x,int y , int z){
+		if (x>=y && x>=z) return x;
+		if (y>=x && y>=z) return y;
+		else return z;
+	}
+
+	
+}
