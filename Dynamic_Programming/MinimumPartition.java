caf3893843e1975b6ff2e3ffcf3bcea4cbe3fd1d
Hiteshi Acharya <acharyahiteshi@gmail.com>
2018-07-12 00:03:40 -0400

    Add files via upload

---
+++ Dynamic_Programming/MinimumPartition.java
@@ -0,0 +1,14 @@
+package Dynamic_Programming;
+/*
+ * 
+ * Given a set of integers, the task is to divide it into two sets S1 and S2 such that the absolute
+ *  difference between their sums is minimum.
+ */
+public class MinimumPartition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
